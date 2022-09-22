9cd56ca84edf9da1faf61abb53c47cad6da93b57
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-22 08:52:36 -0300

    aula 222 fix: Integrating Dashboard with Redux

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { connect } from "react-redux";
 
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
@@ -7,6 +8,7 @@ import Row from "../common/layout/row";
 
 class Dashboard extends Component {
     render() {
+        const {credit, debt} = this.props.summary;
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
@@ -17,7 +19,7 @@ class Dashboard extends Component {
                         cols='12 4'    
                         color='green' 
                         icon='bank'
-                        value='R$ 10'
+                        value={`R$ ${credit}`}
                         text='Total de Créditos'
                     />
 
@@ -25,7 +27,7 @@ class Dashboard extends Component {
                         cols='12 4'    
                         color='red' 
                         icon='credit-card'
-                        value='R$ 10'
+                        value={`R$ ${debt}`}
                         text='Total de Débitos'
                     />
 
@@ -33,7 +35,7 @@ class Dashboard extends Component {
                         cols='12 4'    
                         color='blue' 
                         icon='money'
-                        value='R$ 0'
+                        value={`R$ ${credit - debt}`}
                         text='Valor Consolidado'
                     />
                     
@@ -44,4 +46,5 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard;
+const mapStateToProps = state => ({ summary: state.dashboard.summary})
+export default connect(mapStateToProps)(Dashboard);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
@@ -14,12 +14,13 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __dirname + 'node_modules',
+            modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
-    }
-},
-    puglins: [
+        }
+    },
+    
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
@@ -32,7 +33,7 @@ module.exports = {
             test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
-            qury: {
+            query: {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
