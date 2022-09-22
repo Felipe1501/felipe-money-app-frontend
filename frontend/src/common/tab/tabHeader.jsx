3de700b5829ed35040e6e0f77ec8880d17dc48b2
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-22 11:05:36 -0300

    aula 236 fix: visibilidade das abas pt.2

--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -2,12 +2,15 @@ import React, {Component} from "react";
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
+import If from "../operator/if";
 import {selectTab} from './tabActions';
 
 class TabHeader extends Component {
     render(){
-        const selected = this.props.tab.selected === this.props.target
+        const selected = this.props.tab.selected === this.props.target;
+        const visible = this.props.tab.visible[this.props.target];
         return (
+            <If test={visible}>
             <li className={selected ? 'active' : ''}>
                 <a 
                 href='javascript:;'
@@ -18,6 +21,7 @@ class TabHeader extends Component {
                 {this.props.label} 
                 </a>
             </li>
+            </If>
         )
     }
 }
