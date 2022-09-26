e8b001c1dd3d700f8a004f271a43958a329390e9
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-26 09:17:03 -0300

    aula 274 fix: routes protected and open

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const auth = require('./auth');
 
-module.exports = function(server) {
+module.exports = function (server) {
     //definir URL base para todas as rotas
-    const router = express.Router();
+    /*const router = express.Router();
     server.use('/api', router);
 
     //rotas de ciclo de pagamento
     const BillingCycle = require('../api/billingCycle/billingCycleService');
-    BillingCycle.register(router, '/billingCycles');
+    BillingCycle.register(router, '/billingCycles');*/
+
+
+    //rotas protegidas pelo Token JWT
+    const protectedApi = express.Router();
+    server.use('/api', protectedApi);
+
+    protectedApi.use(auth);
+
+    //rotas de ciclo de pagamento
+    const BillingCycle = require('../api/billingCycle/billingCycleService');
+    BillingCycle.register(protectedApi, '/billingCycles');
+
+    //rotas
+    const openApi = express.Router();
+    server.use('/oapi', openApi);
+
+    const AuthService = require('../api/user/AuthService');
+    openApi.post('/login', AuthService.login);
+    openApi.post('/signup', AuthService.signup);
+    openApi.post('/validateToken', AuthService.validateToken);
+
 }
