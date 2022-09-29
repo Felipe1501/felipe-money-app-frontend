8758d18b0f297f793a40caaae103cf0b8630ded4
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-29 12:07:58 -0300

    fix: imports, and end of project

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,13 +1,13 @@
-import axios from 'axios'
-import { toastr } from 'react-redux-toastr'
-import { reset as resetForm, initialize } from 'redux-form'
-import { showTabs, selectTab } from '../common/tab/tabActions'
+import axios from 'axios';
+import { toastr } from 'react-redux-toastr';
+import { reset as resetForm, initialize } from 'redux-form';
+import { showTabs, selectTab } from '../common/tab/tabActions';
 
-const BASE_URL = 'http://localhost:3003/api'
+const BASE_URL = 'http://localhost:3003/api';
 const INITIAL_VALUES = {credits: [{}], debts: [{}]}
 
 export function getList() {
-    const request = axios.get(`${BASE_URL}/billingCycles`)
+    const request = axios.get(`${BASE_URL}/billingCycles`);
     return {
         type: 'BILLING_CYCLES_FETCHED',
         payload: request
